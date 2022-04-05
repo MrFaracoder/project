a85f407ab3ef6d31d738bc28f12b7610a56f11db
ALi Farahani <mr.facoder@gmail.com>
2022-04-05 19:05:34 +0430

    add tode list

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 Vue.createApp({
     data(){
         return{
-            msg:"hello world"
+            msg:"hello world",
+            dolist:[
+                {
+                    title:"dsfdfdfdff",
+                    completed:false
+                },
+                {
+                    title:"lorem",
+                    completed:true
+                },
+                {
+                    title:"df fgfgf",
+                    completed:false
+                },
+                {
+                    title:"ali farahani",
+                    completed:false
+                },
+            ]
         }
     }
 }).mount("#app")
